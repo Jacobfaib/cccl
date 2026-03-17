0ed149f34540713cace806f63b9b17004faa7fc2
Jacob Faibussowitsch <jacob.fai@gmail.com>
2026-03-17 09:39:36 -0400

    make mdspan no longer type-erase everything

--- cudax/include/cuda/experimental/__sharded_array/__sharded_array.hpp
+++ cudax/include/cuda/experimental/__sharded_array/__sharded_array.hpp
@@ -12,54 +12,125 @@
 
 #include <thrust/device_vector.h>
 
+#include <cuda/__driver/driver_api.h>
+#include <cuda/__iterator/zip_iterator.h>
 #include <cuda/devices>
-#include <cuda/std/__numeric/accumulate.h>
-#include <cuda/std/mdspan>
+#include <cuda/mdspan>
 #include <cuda/std/span>
 #include <cuda/std/variant>
 
 #include <cuda/experimental/__device/logical_device.cuh>
 
+#include <stdexcept>
 #include <vector>
 
 namespace cuda::experimental
 {
-struct host_memory
-{};
+namespace detail
+{
+template <typename... T>
+class overload : public T...
+{
+public:
+  using T::operator()...;
+};
+
+template <typename... T>
+overload(T...) -> overload<T...>;
+} // namespace detail
+
+class host_memory
+{
+public:
+  constexpr bool operator==(const host_memory&) noexcept
+  {
+    return true;
+  }
+
+  constexpr bool operator!=(const host_memory&) noexcept
+  {
+    return false;
+  }
+};
 
 class memory
 {
+  class activated_memory
+  {
+  public:
+    activated_memory() = delete;
+
+    explicit activated_memory(memory& mem)
+        : mem_{&mem}
+    {
+      mem_->activate_();
+    }
+
+    ~activated_memory()
+    {
+      mem_->deactivate_();
+    }
+
+    activated_memory(activated_memory&)            = delete;
+    activated_memory& operator=(activated_memory&) = delete;
+
+  private:
+    memory* mem_{};
+  };
+
+  friend activated_memory;
+
 public:
   memory() = default;
 
   memory(host_memory)
-      : proc_{std::in_place_type<host_memory>}
+      : proc_{::std::in_place_type<host_memory>}
   {}
 
   memory(logical_device dev)
-      : proc_{std::move(dev)}
+      : proc_{::std::move(dev)}
   {}
 
   memory(::cuda::device_ref dev)
       : memory{logical_device{dev}}
   {}
 
+  [[nodiscard]] activated_memory activate_guard()
+  {
+    return activated_memory{*this};
+  }
+
+  [[nodiscard]] bool operator==(const memory& other) const noexcept
+  {
+    return other.proc_ == proc_;
+  }
+
+  [[nodiscard]] bool operator!=(const memory& other) const noexcept
+  {
+    return (*this == other);
+  }
+
 private:
+  void activate_()
+  {
+    ::cuda::std::visit(proc_,
+                       detail::overload{[](const host_memory&) {},
+                                        [](const logical_device& dev) {
+                                          ::cuda::__driver::__ctxPush(dev.context());
+                                        }});
+  }
+
+  void deactivate_()
+  {
+    ::cuda::std::visit(proc_,
+                       detail::overload{[](const host_memory&) {},
+                                        [](const logical_device&) {
+                                          ::cuda::__driver::__ctxPop();
+                                        }});
+  }
+
   ::cuda::std::variant<host_memory, logical_device> proc_{};
 };
-
-template <typename MDSpan>
-class shard
-{
-public:
-  using mdspan_type = MDSpan;
-
-  mdspan_type mdspan{};
-  memory proc{};
-};
-
-template <typename MDSpan>
-explicit shard(const MDSpan&, memory) -> shard<MDSpan>;
 
 template <typename MDSpan>
 class basic_sharded_mdarray
@@ -67,7 +138,13 @@ class basic_sharded_mdarray
 public:
   using mdspan_type = MDSpan;
   using size_type   = typename mdspan_type::size_type;
-  using shard_type  = shard<MDSpan>;
+
+  class shard_type
+  {
+  public:
+    mdspan_type mdspan{};
+    memory proc{};
+  };
 
   [[nodiscard]] ::cuda::std::span<const shard_type> shards() const;
   [[nodiscard]] ::cuda::std::span<shard_type> shards();
@@ -78,6 +155,9 @@ public:
 private:
   ::std::vector<shard_type> shards_{};
 };
+
+template <typename Shard, typename... Rest>
+basic_sharded_mdarray(Shard, Rest...) -> basic_sharded_mdarray<typename Shard::mdspan_type>;
 
 template <typename M>
 ::cuda::std::span<const typename basic_sharded_mdarray<M>::shard_type> basic_sharded_mdarray<M>::shards() const
@@ -112,16 +192,23 @@ bool basic_sharded_mdarray<M>::empty() const
 // ==========================================================================================
 
 template <typename MDSpan, typename... Rest>
-basic_sharded_mdarray<MDSpan> make_sharded_mdarray(shard<MDSpan> first, Rest&&... rest)
+basic_sharded_mdarray<MDSpan> make_sharded_mdarray(typename basic_sharded_mdarray<MDSpan>::shard first, Rest&&... rest)
 {
   return basic_sharded_mdarray<MDSpan>{std::move(first), std::move(rest)...};
 }
 
 template <typename MDSpan, typename F>
-inline void transform(const basic_sharded_mdarray<MDSpan>& mdarray, F&& functor)
+void transform(const basic_sharded_mdarray<MDSpan>& in_mdarray,
+               const basic_sharded_mdarray<MDSpan>& out_mdarray,
+               F&& functor)
 {
-  for (auto&& shard : mdarray.shards())
+  auto zipper = make_zip_iterator(in_mdarray.shards(), out_mdarray.shards());
+
+  for (auto&& [in_shard, out_shard] : zipper)
   {
+    const auto _ = in_shard.proc.activate_guard();
+
+    thrust::transform(in_shard.subspan, out_shard.subspan, std::forward<F>(functor));
   }
 }
 
@@ -131,10 +218,10 @@ inline void foo()
   auto v2 = thrust::device_vector<int>{4};
 
   auto v1_span = ::cuda::std::mdspan{thrust::raw_pointer_cast(v1.data()), 1, 2};
-  auto v2_span = ::cuda::std::mdspan{thrust::raw_pointer_cast(v2.data()), 4, 5};
+  auto v2_span = ::cuda::device_mdspan{thrust::raw_pointer_cast(v2.data()), 4, 5};
 
-  auto sharded = make_sharded_mdarray(shard{v1_span, logical_device{0}}, shard{v2_span, host_memory{}});
+  auto sharded = basic_sharded_mdarray{{v1_span, logical_device{0}}, {v2_span, host_memory{}}};
 
-  transform(sharded, [] {});
+  transform(sharded, sharded, [] {});
 }
 } // namespace cuda::experimental
