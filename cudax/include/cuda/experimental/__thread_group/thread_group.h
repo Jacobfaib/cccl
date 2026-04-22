f9ecd5dca6c21327a6b96283056af51e897fd1bd
Jacob Faibussowitsch <jacob.fai@gmail.com>
2026-04-22 09:52:53 -0400

    fixup! Add cuda::experimental::thread_group

--- cudax/include/cuda/experimental/__thread_group/thread_group.h
+++ cudax/include/cuda/experimental/__thread_group/thread_group.h
@@ -21,7 +21,6 @@
 #  pragma system_header
 #endif // no system header
 
-#include <cuda/__device/all_devices.h>
 #include <cuda/std/__barrier/barrier.h>
 #include <cuda/std/__cstddef/byte.h>
 #include <cuda/std/__cstddef/types.h>
@@ -31,11 +30,8 @@
 #include <cuda/std/atomic>
 #include <cuda/std/span>
 
-#include <cuda/experimental/__device/logical_device.cuh>
-
 #include <memory>
 #include <stdexcept>
-#include <vector>
 
 #include <cuda/std/__cccl/prologue.h>
 
@@ -43,92 +39,120 @@ namespace cuda::experimental
 {
 class thread_group
 {
-public:
-  using size_type = ::cuda::std::size_t;
-  using rank_type = ::cuda::std::size_t;
-
-private:
   static constexpr ::cuda::std::byte __MAGIC_INIT_VALUE{222};
   static constexpr ::cuda::std::byte __MAGIC_CONSTRUCTED_VALUE{0};
 
   class __shared_data
   {
-  public:
-    __shared_data() = delete;
+    ::cuda::std::byte __constructed_flag;
+    ::cuda::std::atomic<::cuda::std::uint32_t> __destroy_count;
+    ::cuda::std::barrier<> __bar;
+    ::cuda::std::span<::cuda::std::byte> __scratch_mem;
 
-    _CCCL_HOST_API explicit __shared_data(
-      const ::cuda::std::byte* __raw_ptr, size_type __group_size, ::cuda::std::span<::cuda::std::byte> __mem)
+  public:
+    _CCCL_HOST_API explicit __shared_data([[maybe_unused]] const ::cuda::std::byte* __raw_ptr,
+                                          ::cuda::std::ptrdiff_t __group_size,
+                                          ::cuda::std::span<::cuda::std::byte> __mem)
         // Do not initialize __constructed_flag in initializer list, it must be initialized
         // atomically
-        : __barrier{static_cast<::cuda::std::ptrdiff_t>(__group_size)}
+        : __destroy_count{static_cast<::cuda::std::uint32_t>(__group_size)}
+        , __bar{__group_size}
         , __scratch_mem{__mem}
     {
       // We need the __constructed_flag member to sit at exactly the first byte of the shared
       // memory buffer (not __mem, the original buffer), so that the other threads can be
-      // properly notified that we have constructed ourselves. If that is ever not the case,
-      // then we will write to the wrong memory location and never release the waiting threads.
+      // properly notified that we have constructed ourselves. This is also why we use a raw
+      // byte, and not a atomic<byte>, because we cannot guarantee where the atomic
+      // implementation will place the value inside the struct.
+      //
+      // If this value is ever not at the very first byte, then we will write to the wrong
+      // memory location and never release the waiting threads.
       _CCCL_ASSERT(&__constructed_flag == __raw_ptr,
                    "__shared_data constructed at an offset inside shared memory scratch buffer");
 
       auto __ref = ::cuda::std::atomic_ref<::cuda::std::byte>{__constructed_flag};
 
-      __ref = __MAGIC_CONSTRUCTED_VALUE;
+      __ref.store(__MAGIC_CONSTRUCTED_VALUE, ::cuda::std::memory_order_release);
       __ref.notify_all();
     }
 
-    ::cuda::std::byte __constructed_flag;
-    ::cuda::std::barrier<> __barrier;
-    ::cuda::std::span<::cuda::std::byte> __scratch_mem;
+    _CCCL_HOST_API bool __can_destroy() noexcept
+    {
+      return __destroy_count.fetch_sub(1, ::cuda::std::memory_order_release) == 1;
+    }
+
+    [[nodiscard]] _CCCL_HOST_API ::cuda::std::barrier<>& __barrier() noexcept
+    {
+      return __bar;
+    }
+
+    [[nodiscard]] _CCCL_HOST_API const ::cuda::std::barrier<>& __barrier() const noexcept
+    {
+      return __bar;
+    }
+
+    template <typename T>
+    [[nodiscard]] _CCCL_HOST_API ::cuda::std::span<T> __scratch_mem_as() noexcept
+    {
+      auto* const ptr = reinterpret_cast<T*>(__scratch_mem.data());
+
+      return {ptr, __scratch_mem.size() / sizeof(T)};
+    }
+
+    template <>
+    [[nodiscard]] _CCCL_HOST_API constexpr ::cuda::std::span<::cuda::std::byte> __scratch_mem_as() noexcept
+    {
+      return __scratch_mem;
+    }
+
+    template <>
+    [[nodiscard]] _CCCL_HOST_API constexpr ::cuda::std::span<const ::cuda::std::byte> __scratch_mem_as() const noexcept
+    {
+      return __scratch_mem;
+    }
   };
 
 public:
-  _CCCL_HOST_API thread_group(rank_type __rank,
-                              size_type __size,
-                              const ::cuda::__all_devices& __all_dev,
-                              ::std::unique_ptr<::cuda::std::byte[]> __shared_mem)
-      : thread_group{__rank, __size, {__all_dev.begin(), __all_dev.end()}, ::cuda::std::move(__shared_mem)}
-  {}
-
-  _CCCL_HOST_API thread_group(rank_type __rank,
-                              size_type __size,
-                              ::std::vector<logical_device> __devices,
-                              ::std::unique_ptr<::cuda::std::byte[]> __shared_mem)
+  _CCCL_HOST_API thread_group(::cuda::std::uint32_t __rank,
+                              ::cuda::std::uint32_t __group_size,
+                              ::cuda::std::uint32_t __num_gpus,
+                              ::std::shared_ptr<::cuda::std::byte[]> __shared_mem)
       : __rank_{__rank}
-      , __size_{__size}
-      , __devices_{::cuda::std::move(__devices)}
-      , __shared_mem_{::cuda::std::move(__shared_mem)}
+      , __group_size_{__group_size}
+      , __shared_mem_{construct_shared_data_(__rank, __group_size, ::cuda::std::move(__shared_mem))}
   {
-    // Technically only thread 0 needs to perform any of the pointer aligning, but we let all
-    // threads do it so we can error-check the arguments properly.
-    auto* const __raw_ptr         = __shared_mem_.get();
-    const auto __min_scratch_size = required_scratch_mem_size_(size());
-    auto* __scratch_ptr           = static_cast<void*>(__raw_ptr + sizeof(__shared_data));
-    auto __capacity               = required_shared_memory_size(size()) - sizeof(__shared_data);
-
-    if (!::cuda::std::align(required_scratch_mem_alignment_(), __min_scratch_size, __scratch_ptr, __capacity))
+    _CCCL_TRY
     {
-      _CCCL_THROW(::std::length_error,
-                  "Failed to align scratch memory pointer. Likely allocated shared memory is insufficient.");
+      const auto __u32_scratch = shared_data_()->__scratch_mem_as<::cuda::std::uint32_t>();
+      const auto __ref         = ::cuda::std::atomic_ref<::cuda::std::uint32_t>{__u32_scratch[rank()]};
+
+      __ref.store(__num_gpus, ::cuda::std::memory_order_relaxed);
+
+      barrier();
+
+      for (::cuda::std::uint32_t __i = 0; __i < size(); ++__i)
+      {
+        const auto __s = __u32_scratch[__i];
+
+        if (__i < rank())
+        {
+          __sub_idx_begin_ += __s;
+        }
+        __stride_ += __s;
+      }
+      __sub_idx_end_ = __sub_idx_begin_ + __num_gpus;
     }
-
-    // Construct last, to preserve strong exception guarantee.
-    if (rank() == 0)
+    _CCCL_CATCH_ALL
     {
-      auto* const __ptr = reinterpret_cast<__shared_data*>(__raw_ptr);
-      auto __span = ::cuda::std::span<::cuda::std::byte>{static_cast<::cuda::std::byte*>(__scratch_ptr), __capacity};
-
-      ::cuda::std::__construct_at(__ptr, __raw_ptr, size(), __span);
-    }
-    else
-    {
-      // Cannot use barrier because the barrier may not yet be constucteed. Instead, we use the
-      // first byte of the shared memory to
-      auto __ref = ::cuda::std::atomic_ref<::cuda::std::byte>{__shared_mem[0]};
-
-      __ref.wait(__MAGIC_INIT_VALUE);
+      // Strong exception guarantee. There is an argument to be made here to just call
+      // cuda::std::terminate(), because if any of the threads throw, then the program *will*
+      // eventually deadlock on a barrier.
+      __reset();
+      _CCCL_RETHROW;
     }
   }
 
+  // This object may not be copied because __reset()
   _CCCL_HIDE_FROM_ABI thread_group(const thread_group&)            = delete;
   _CCCL_HIDE_FROM_ABI thread_group& operator=(const thread_group&) = delete;
 
@@ -137,40 +161,39 @@ public:
 
   _CCCL_HOST_API ~thread_group()
   {
-#ifndef NDEBUG
-    barrier();
-#endif
-    if (rank() == 0)
-    {
-      ::cuda::std::destroy_at(&shared_data_());
-    }
+    __reset();
   }
 
-  [[nodiscard]] _CCCL_HOST_API constexpr rank_type rank() const noexcept
+  [[nodiscard]] _CCCL_HOST_API constexpr ::cuda::std::uint32_t rank() const noexcept
   {
     return __rank_;
   }
 
-  [[nodiscard]] _CCCL_HOST_API constexpr size_type size() const noexcept
+  [[nodiscard]] _CCCL_HOST_API constexpr ::cuda::std::uint32_t size() const noexcept
   {
-    return __size_;
+    return __group_size_;
+  }
+
+  [[nodiscard]] _CCCL_HOST_API constexpr ::cuda::std::uint32_t index_begin() const noexcept
+  {
+    return __sub_idx_begin_;
+  }
+
+  [[nodiscard]] _CCCL_HOST_API constexpr ::cuda::std::uint32_t index_end() const noexcept
+  {
+    return __sub_idx_end_;
   }
 
   _CCCL_HOST_API void barrier() const
   {
     if (size() > 1)
     {
-      mut_shared_data_().__barrier.arrive_and_wait();
+      mut_shared_data_()->__barrier().arrive_and_wait();
     }
   }
 
-  [[nodiscard]] _CCCL_HOST_API constexpr ::cuda::std::span<const logical_device> devices() const noexcept
-  {
-    return __devices_;
-  }
-
   [[nodiscard]] _CCCL_API static constexpr ::cuda::std::size_t
-  required_shared_memory_size(size_type __group_size) noexcept
+  required_shared_memory_size(::cuda::std::uint32_t __group_size) noexcept
   {
     return sizeof(__shared_data) + alignof(__shared_data) - 1 + required_scratch_mem_size_(__group_size);
   }
@@ -186,43 +209,90 @@ public:
     }
     else
     {
-      ::cuda::std::atomic_ref{*static_cast<::cuda::std::byte*>(__mem)} = __MAGIC_INIT_VALUE;
+      ::cuda::std::atomic_ref{*static_cast<::cuda::std::byte*>(__mem)}.store(
+        __MAGIC_INIT_VALUE, ::cuda::std::memory_order_release);
     }
     return __mem;
   }
 
 private:
   [[nodiscard]] _CCCL_API static constexpr ::cuda::std::size_t
-  required_scratch_mem_size_(size_type __group_size) noexcept
+  required_scratch_mem_size_(::cuda::std::uint32_t __group_size) noexcept
   {
-    // All members of the group have a void*'s worth of private memory
-    return __group_size * sizeof(void*);
+    // All members of the group have a uint64_t's worth of private memory
+    return __group_size * sizeof(::cuda::std::uint64_t);
   }
 
-  [[nodiscard]] _CCCL_API static constexpr ::cuda::std::size_t required_scratch_mem_alignment_() noexcept
+  [[nodiscard]] _CCCL_API static constexpr ::cuda::std::uint32_t required_scratch_mem_alignment_() noexcept
   {
     return alignof(::cuda::std::max_align_t);
   }
 
-  [[nodiscard]] _CCCL_HOST_API __shared_data& shared_data_() noexcept
+  [[nodiscard]] _CCCL_HOST_API __shared_data* shared_data_() noexcept
   {
-    return reinterpret_cast<__shared_data&>(*__shared_mem_.get());
+    return reinterpret_cast<__shared_data*>(__shared_mem_.get());
   }
 
-  [[nodiscard]] _CCCL_HOST_API const __shared_data& shared_data_() const noexcept
+  [[nodiscard]] _CCCL_HOST_API const __shared_data* shared_data_() const noexcept
   {
-    return reinterpret_cast<const __shared_data&>(*__shared_mem_.get());
+    return reinterpret_cast<const __shared_data*>(__shared_mem_.get());
   }
 
-  [[nodiscard]] _CCCL_HOST_API __shared_data& mut_shared_data_() const noexcept
+  [[nodiscard]] _CCCL_HOST_API __shared_data* mut_shared_data_() const noexcept
   {
     return const_cast<thread_group&>(*this).shared_data_();
   }
 
-  rank_type __rank_{};
-  size_type __size_{};
-  ::std::vector<logical_device> __devices_{};
-  ::std::unique_ptr<::cuda::std::byte[]> __shared_mem_{};
+  _CCCL_HOST_API void __reset() noexcept
+  {
+    // We could have been moved-from, so need to test __sd
+    if (auto* const __sd = shared_data_(); __sd && __sd->__can_destroy())
+    {
+      ::cuda::std::destroy_at(__sd);
+    }
+  }
+
+  [[nodiscard]] _CCCL_HOST_API static ::std::shared_ptr<::cuda::std::byte[]> construct_shared_data_(
+    ::cuda::std::uint32_t __rank, ::cuda::std::uint32_t __size, ::std::shared_ptr<::cuda::std::byte[]> __shared_mem)
+  {
+    // Technically only thread 0 needs to perform any of the pointer aligning, but we let all
+    // threads do it so we can error-check the arguments properly.
+    auto* const __raw_ptr         = __shared_mem.get();
+    const auto __min_scratch_size = required_scratch_mem_size_(__size);
+    auto* __scratch_ptr           = static_cast<void*>(__raw_ptr + sizeof(__shared_data));
+    auto __capacity               = required_shared_memory_size(__size) - sizeof(__shared_data);
+
+    if (!::cuda::std::align(required_scratch_mem_alignment_(), __min_scratch_size, __scratch_ptr, __capacity))
+    {
+      _CCCL_THROW(::std::length_error,
+                  "Failed to align scratch memory pointer. Likely allocated shared memory is insufficient.");
+    }
+
+    // Construct last, to preserve strong exception guarantee.
+    if (__rank == 0)
+    {
+      auto* const __ptr = reinterpret_cast<__shared_data*>(__raw_ptr);
+      auto __span = ::cuda::std::span<::cuda::std::byte>{static_cast<::cuda::std::byte*>(__scratch_ptr), __capacity};
+
+      ::cuda::std::__construct_at(__ptr, __raw_ptr, static_cast<::cuda::std::ptrdiff_t>(__size), __span);
+    }
+    else
+    {
+      // Cannot use barrier because the barrier may not yet be constucteed. Instead, we use the
+      // first byte of the shared memory to
+      auto __ref = ::cuda::std::atomic_ref<::cuda::std::byte>{__shared_mem[0]};
+
+      __ref.wait(__MAGIC_INIT_VALUE, ::cuda::std::memory_order_acquire);
+    }
+    return __shared_mem;
+  }
+
+  ::cuda::std::uint32_t __rank_{};
+  ::cuda::std::uint32_t __group_size_{};
+  ::cuda::std::uint32_t __sub_idx_begin_{};
+  ::cuda::std::uint32_t __sub_idx_end_{};
+  ::cuda::std::uint32_t __stride_{};
+  ::std::shared_ptr<::cuda::std::byte[]> __shared_mem_{};
 };
 } // namespace cuda::experimental
 
